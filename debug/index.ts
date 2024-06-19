b179fc18cb86f8ed1481422d8cb7466d81c859b1
rharkor <louis@huort.com>
2024-06-19 21:21:50 +0200

    feat: custom sort

--- debug/index.ts
+++ debug/index.ts
@@ -13,10 +13,14 @@ const main = async () => {
     poolId: "clxh8j8ua0001wact0t562zjs",
     encryptionKey: process.env.SMART_DEV_ENCRYPTION_KEY as string,
     smartContractId: "clwt0th7q000036g9cak5rp6g",
-    method: "envoyerFonds(address)",
-    methodParams: ["0x3123D1CC0A6197B93d05Cf410B8218E10C0E932a"],
-    transactionParams: {
-      value: "1",
+    method: "tupleInput((string, uint256, bool))",
+    methodParams: [{ status: "test", another: 10, valid: true }],
+    customSort: {
+      smartContractId: "clwt0th7q000036g9cak5rp6g",
+      method: "getRandomNumber(uint256)",
+      direction: "asc",
+      resultKind: "bigint",
+      methodParams: [10],
     },
   })
 
